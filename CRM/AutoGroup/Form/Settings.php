c61ec69af6113bf0aefa0b44313a0bee63974b82
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2022-01-18 14:08:30 +0000

    Fix #3 only top 25 groups listed on settings page

--- CRM/AutoGroup/Form/Settings.php
+++ CRM/AutoGroup/Form/Settings.php
@@ -31,7 +31,11 @@ class CRM_AutoGroup_Form_Settings extends CRM_Admin_Form_Setting {
     $e->setSize(12);
 
     // Get list of groups (we exclude mailing or access groups).
-    $result = civicrm_api3('group', 'get', ['return' => 'title', 'group_type' => ['IS NULL' => 1], 'options' => ['sort' => 'title']]);
+    $result = civicrm_api3('group', 'get', [
+      'return' => 'title',
+      'group_type' => ['IS NULL' => 1],
+      'options' => ['sort' => 'title', 'limit' => 0]
+    ]);
     foreach ($result['values'] as $_) {
       $e->addOption($_['title'], $_['id']);
     }
